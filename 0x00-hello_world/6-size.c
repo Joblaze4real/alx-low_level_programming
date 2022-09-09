73ed23f2116d0a69e3c55d9a8b8bf8f1216d13f0
joblaze4real <joblaze4real@gmail.com>
2022-09-09 14:05:53 +0000

    Different size types C program completed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,25 +4,24 @@
  * in the put function
  * Description: using the main function, this program prints "different types
  * of size on the computer it is compiled and run on
- * Return: 0
- */
-/*
+ *
  * First code Tested but to no avail
  *
  * int main(void)
-{
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;	
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of a int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
-}
+ * {
+ * char c;
+ * int i;
+ * long li;
+ * long long lli;
+ * float f;
+ * printf("Size of a char: %ld byte(s)\n", sizeof(c));
+ * printf("Size of a int: %ld byte(s)\n", sizeof(i));
+ * printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+ * printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+ * printf("Size of a float: %ld byte(s)\n", sizeof(f));
+ * return (0);
+ * }
+ * Return: Always 0(Success)
  */
 int main(void)
 {
