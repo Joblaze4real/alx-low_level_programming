7d4576e5d62e886390fcd203108a06bac7149f60
joblaze4real <joblaze4real@gmail.com>
2022-09-09 13:56:42 +0000

    Different size types C program completed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,10 @@
  * of size on the computer it is compiled and run on
  * Return: 0
  */
-int main(void)
+/*
+ * First code Tested but to no avail
+ *
+ * int main(void)
 {
 	char c;
 	int i;
@@ -18,5 +21,15 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
