98a0e321eb0e491cda686dfe036bd578ea58d7aa
joblaze4real <joblaze4real@gmail.com>
2022-09-13 17:08:08 +0000

    Task completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Function that prints the alphabet, in lowercase.
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	char = 'a';
+
+	while (char <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+}
