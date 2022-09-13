a33acf3929ffa2ae9fb1a1fbb32e623e8f204b7b
joblaze4real <joblaze4real@gmail.com>
2022-09-13 16:17:58 +0000

    Task completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints _putchar followed by a new line
+ * Return: 0 if successfull
+ */
+int main(void)
+{
+	char p = _putchar;
+	int n;
+
+	while (n < 2)
+	{
+		putchar(p'\n');
+	}
+	putchar('\n');
+	return (0);
+}
