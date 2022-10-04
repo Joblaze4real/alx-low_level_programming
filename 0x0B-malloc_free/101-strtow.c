c7d77861d8af103e9e70b1b341474f127279ad70
joblaze4real <joblaze4real@gmail.com>
2022-10-04 16:32:07 +0000

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -21,11 +21,11 @@ void ch_free_grid(char **grid, unsigned int height)
 }
 
 /**
- *  * strtow - splits a string into words.
- *   * @str: string.
- *    *
- *     * Return: pointer of an array of integers
- *      */
+ * **strtow - splits a string into words.
+ * @str: string.
+ * Return: pointer of an array of integers
+ */
+
 char **strtow(char *str)
 {
 	char **aout;
