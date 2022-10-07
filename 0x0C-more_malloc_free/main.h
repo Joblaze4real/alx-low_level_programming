cd65d7c6bc6ab1cdbc49da8ed9bfc00fc6d3687d
joblaze4real <joblaze4real@gmail.com>
2022-10-07 12:10:49 +0000

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,6 +10,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
+void _is_zero(char *argv[]);
+char *_initialize_array(char *ar, int lar);
+int _checknum(char *argv[], int n);
+int main(int argc, char *argv[])
 
 #endif
 
