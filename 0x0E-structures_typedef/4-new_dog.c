10ee2f011dc6fd77cc7adc8aba622c272970cad3
Joshua Roland <joblaze4real@gmail.com>
2022-10-11 06:07:50 -0700

    Task completed!

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,51 +1,76 @@
-#include <stdlib.h>
 #include "dog.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
+ * new_dog - create new data structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
  *
- * Return: the length of the string
- */
-int _strlen(char *s)
+ * Return: Pointer to new dog
+ **/
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
+	dog_t *new_dog;
+	int len_name, len_owner;
 
-	i = 0;
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-	while (s[i] != '\0')
+	len_name = _strlen(name);
+	new_dog->name = malloc(sizeof(char) * len_name + 1);
+	if (new_dog->name == NULL)
 	{
-		i++;
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->name = _strcpy(new_dog->name, name);
+	len_owner = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
 	}
 
-	return (i);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+
+	return (new_dog);
 }
 
 /**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
+ * _strlen - determinates the lenght of a string
+ * @s: pointer to string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	return (a);
+}
+
+/**
+ * _strcpy - copies a pointed string
+ * @dest: pointer to the destine string
+ * @src: pointer to the source string
+ * Return: the adress of the destiny string
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int a = 0;
 
-	len = 0;
-
-	while (src[len] != '\0')
+	while (src[a] != '\0')
 	{
-		len++;
+		dest[a] = src[a];
+		a++;
 	}
 
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
+	dest[a] = '\0';
 	return (dest);
 }
